7bb6f26ca5e3dea7f0532588e7cec944bd668a8e
TheGreatday21 <kaizzielijah@gmail.com>
2024-09-12 17:59:32 +0300

    day 1

---
+++ week_2/first_js_program.js
@@ -0,0 +1,7 @@
+//This is my first java script programm//
+console.log("Hello world");
+
+//python equivalent is print()
+console.log("Kawooma Elijah Kaizzi");
+console.log("M24B38/023");
+console.log("B29158");
---
+++ week_2/variables.js
@@ -0,0 +1,57 @@
+//comments in js are written by using"//"//
+//All the code ends with the same notation like in c ";"//
+//to comment multiple lines use /* */
+//the print of java is console.log//
+//variables//
+
+/*in pYTHON//
+
+AGE = 23
+HEIGHT = 123.4
+*/
+
+//in Java script
+// we can first declare the variable 
+var  height ;
+var first_name;
+//then we can assign the variable values
+height = 145.8;
+first_name = "Libpo";
+console.log(height);
+console.log(first_name ,"=", height);
+
+//There are 3 ways of defining a variable 
+//// 1 - Using the Key word Var
+var my_name = "Kawooma Elijah";
+var my_age = 45;
+console.log(my_age,my_name);
+
+////2. The second way is using the "let" function
+let area = 90.5
+let f_name = "keks"
+console.log ("Name",f_name);
+console.log("Area", area)
+
+////3. Using the "const"
+const pie = 3.14;
+//can't assign under const
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+e 
