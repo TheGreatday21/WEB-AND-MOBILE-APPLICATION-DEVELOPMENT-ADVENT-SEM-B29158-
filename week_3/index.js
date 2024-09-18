43b27ef31a971bf16afd5c72242bfa0818102d67
TheGreatday21 <kaizzielijah@gmail.com>
2024-09-18 15:05:08 +0300

    new_work

---
+++ week_3/index.js
@@ -0,0 +1,17 @@
+//reviewing what we know in js//
+console.log("Hello world");
+//creating variables //
+let first_name = "keks";
+let last_name = "the Greatday 21";
+
+const PIE =  3.1423;
+const MAX_SPEED = 9000;
+
+let  is_student = false;//This is bool in java//
+
+var school_name = "UCU"
+
+
+
+
+
