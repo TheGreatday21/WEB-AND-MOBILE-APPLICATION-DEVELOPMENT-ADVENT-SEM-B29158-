a90633dd17139983d12d9ace48487253dc0cd250
TheGreatday21 <kaizzielijah@gmail.com>
2024-09-18 15:54:19 +0300

    the tech queens codequest

---
+++ week_3/tech_queens _tech_quest.js
@@ -0,0 +1,28 @@
+//Zaras challenge//
+let queenName =  "Esther";
+let completedChallenges = 0;
+
+completedChallenges += 1;
+
+console.log(completedChallenges);
+
+//Amina's Amazing challenges//
+function celebrateWin(str){
+    console.log(str);
+    const challengeName = prompt("ChallengeName: ")
+        console.log(challengeName,+ "completed")
+    return console.log("Time to take the next challenge");
+   
+}
+console.log(celebrateWin("You did it Queen"));
+
+
+
+
+
+
+
+
+
+
+
