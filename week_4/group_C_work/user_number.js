3d8eada07b8f28a86744d8f8b72dc2f7fbff68e5
TheGreatday21 <kaizzielijah@gmail.com>
2024-09-25 16:25:23 +0300

    retail

---
+++ week_4/group_C_work/user_number.js
@@ -0,0 +1,37 @@
+let number_of_users = 170;
+
+if(number_of_users > 100){
+    console.log("Network is overloaded");
+}
+else{
+    while(number_of_users+10){
+        if(number_of_users <100){
+        console.log("The system is stable");
+        }
+        else{
+            console.log("System is not stable")
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
