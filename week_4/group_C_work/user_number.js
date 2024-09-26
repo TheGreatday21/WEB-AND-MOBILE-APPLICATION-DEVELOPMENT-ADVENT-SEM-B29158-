66152cc2b6dba9ec63cc88cb68451c6741bbeab6
TheGreatday21 <kaizzielijah@gmail.com>
2024-09-26 08:38:11 +0300

    new

--- week_4/group_C_work/user_number.js
+++ week_4/group_C_work/user_number.js
@@ -4,7 +4,7 @@ if(number_of_users > 100){
     console.log("Network is overloaded");
 }
 else{
-    while(number_of_users+10){
+    while(number_of_users<100){
         if(number_of_users <100){
         console.log("The system is stable");
         }
