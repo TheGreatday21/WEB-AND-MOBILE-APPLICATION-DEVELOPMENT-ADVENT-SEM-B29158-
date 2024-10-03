eeb39b3ac0ace46c0fe80db26ad0f09ffd66e5a2
TheGreatday21 <kaizzielijah@gmail.com>
2024-10-03 14:03:47 +0300

    class_work

---
+++ week_5/Group_work/profit.js
@@ -0,0 +1,2 @@
+//write a function calculateProfit that accepts the selling price, cost price, 
+//and the number of units sold, and returns the profit for small business selling products like maize flour
