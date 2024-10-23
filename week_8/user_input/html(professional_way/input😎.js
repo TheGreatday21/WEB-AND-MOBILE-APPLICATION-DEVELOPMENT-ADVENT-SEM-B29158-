928ce7b501ba4dffcedd6de47d4b1ba7b94edee2
TheGreatday21 <kaizzielijah@gmail.com>
2024-10-23 11:55:49 +0300

    First_react_apps

---
+++ week_8/user_input/html(professional_way/input😎.js
@@ -0,0 +1,2 @@
+let username  = window.prompt("what is your name nigga ");
+console.log(username)
---
+++ week_8/user_input/windows_prompt(easyway)/userinput😅.js
@@ -0,0 +1,44 @@
+/*In java script there are 2 ways to recieve user input
+1. Using window prompts  -- LESS PROFESSIONAL BUT EASIEST
+2. Using HTML    -- MORE PROFESSIONAL
+*/
+//////////////WINDOW PROMPTS/////////////////////
+
+//We first let all the variables we are to use in  the code//
+
+let username ;
+let age;
+let height;
+//We then assign the variables using `window ` prompts to receive user input 
+
+
+username = window.prompt("What is your name");
+console.log(username);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
